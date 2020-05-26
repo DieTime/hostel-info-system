78ca099cf1e2d8610e819b0a76f04ede397e1d52
DieTime <gldenis00@mail.ru>
2020-05-27 00:56:50 +0300

    Add build

---
+++ client/build/precache-manifest.72ca4091583a326af3d91fbf86d4ee7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d58b9b81f0cfffb83b07666d70b46bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "60e3de79ec9f9624f828",
+    "url": "./static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "0694bb9e76016168b024",
+    "url": "./static/css/main.3777eb6d.chunk.css"
+  },
+  {
+    "revision": "60e3de79ec9f9624f828",
+    "url": "./static/js/2.41c3efe1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.41c3efe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0694bb9e76016168b024",
+    "url": "./static/js/main.579ee521.chunk.js"
+  },
+  {
+    "revision": "248877f4cfcdba0f1612",
+    "url": "./static/js/runtime-main.a52eee88.js"
+  }
+]);
