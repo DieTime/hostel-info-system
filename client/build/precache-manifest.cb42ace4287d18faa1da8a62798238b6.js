5385ebf82a1ee7e2f916595a3097aed35a99908d
DieTime <gldenis00@mail.ru>
2020-05-27 15:23:37 +0300

    Switch name and surname

---
+++ client/build/precache-manifest.cb42ace4287d18faa1da8a62798238b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc520f0c71395bef09d01363d162ff04",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7321d4d82e75a3f12e52",
+    "url": "./static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "d567dcdf79559aa4c5eb",
+    "url": "./static/css/main.3777eb6d.chunk.css"
+  },
+  {
+    "revision": "7321d4d82e75a3f12e52",
+    "url": "./static/js/2.89dfd74e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.89dfd74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d567dcdf79559aa4c5eb",
+    "url": "./static/js/main.21e02f03.chunk.js"
+  },
+  {
+    "revision": "248877f4cfcdba0f1612",
+    "url": "./static/js/runtime-main.a52eee88.js"
+  }
+]);
